072083631dfbd894a78eff8eb88b03256af806d9
Enes Güler <kriptoenes@gmail.com>
2022-02-16 16:38:35 +0300

    Now it's waiting for each frame. 'p' key for passing space key for saving.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import os
 
-video_name = 'roadview.mp4'
+video_name = 'output_6.avi'
 directory_name = ''.join(video_name.split('.')[:-1])
 parent_path = os.getcwd()
 exact_path = os.path.join(parent_path, directory_name)
@@ -12,9 +12,13 @@ cap = cv.VideoCapture(video_name)
 count = 100000
 while True:
 	ret, frame = cap.read()
-	cv.imshow("out", frame)
+	if ret:
+		cv.imshow("out", frame)
 
-	key = cv.waitKey(1) & 0xFF
-	if key == ord(" "):
-		cv.imwrite(exact_path + '/image' + str(count) + '.jpg', frame)
-		count = count + 1
+		key = cv.waitKey() & 0xFF
+		if key == ord(" "):
+			cv.imwrite(exact_path + '/image' + str(count) + '.jpg', frame)
+			count = count + 1
+		if key == ord("p"):
+			pass
+
